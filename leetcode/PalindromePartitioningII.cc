54ead4b104f91fc6c31b4c173f595351518106ce
caogeng <yangbaoxing@gmail,com>
2013-06-28 09:13:44 +0800

    update palindromepartioningII

--- leetcode/PalindromePartitioningII.cc
+++ leetcode/PalindromePartitioningII.cc
@@ -15,51 +15,45 @@
  *
  * =====================================================================================
  */
-
 class Solution {
 public:
     int minCut(string s) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
         int s_len = s.size();
-        vector<int> tmp(s_len, -1);
-        vector<vector<int>> is_pal(s_len, tmp);
-        return minCut(s, 0, s.size() - 1, is_pal);
+        // -1 unknown
+        vector<int> min_cut(s_len, -1);
+        return minCut(s, 0, s_len - 1, min_cut);
     }
     
-    int minCut(string s, int start, int end, vector<vector<int>>& is_pal) {
-        // 0 false
-        // 1 true
-        // -1 unknown
+    int minCut(string& s, int start, int end, vector<int>& min_cut) {
         int ret = end - start;
-        for (int i = start; i <= end; i++) {
-            string s1 = s.substr(start, i - start + 1);
-            bool flag;
-            int t = is_pal[start][i];
-            if(t != -1)
-                flag = t;
-            else {
-                flag = is_palindrome(s1);
-                is_pal[start][i] = (int)flag;
-            }
-            if (!flag)
+        for (int i = end; i >= start; i--) {
+            if (!is_palindrome(s, start, i))
                 continue;
             if (i == end) {
                 return 0;
             }
-            int tmp = minCut(s, i+1, end, is_pal);
+            int tmp;
+            if (min_cut[i+1] != -1)
+                tmp = min_cut[i+1];
+            else {
+                tmp = minCut(s, i+1, end, min_cut);
+                min_cut[i+1] = tmp;
+            }
+            if (tmp == 0)
+                return 1;
             if (ret > (tmp + 1))
                 ret = tmp + 1;
         }
         return ret;
     }
     
-    inline bool is_palindrome(string s) {
-        int s_len = s.size();
-        int n1 = s_len / 2;
-        int n2 = s_len -1;
-        for (int i = 0; i < n1; i++) {
-            if (s[i] != s[n2 - i])
+    bool is_palindrome(string& s, int start, int end) {
+        int p1 = start;
+        int p2 = end;
+        while (p1 < p2) {
+            if (s[p1++] != s[p2--])
                 return false;
         }
         return true;
